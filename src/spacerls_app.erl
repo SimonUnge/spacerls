b9775d3fc4dd275ca906af802fcb6e8ade150860
Simon Unge <simonunge@gmail.com>
2012-11-27 22:11:55 +0100

    first commit

---
+++ src/spacerls_app.erl
@@ -0,0 +1,16 @@
+-module(spacerls_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    spacerls_sup:start_link().
+
+stop(_State) ->
+    ok.
